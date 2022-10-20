77664da6816d0b817a3ba70a115badb94885e157
egreenfield323 <evangreenfield@live.com>
2022-10-20 14:27:43 -0400

    project file and evan's method
    hi

---
+++ bigOActivity/src/BigO.java
@@ -0,0 +1,12 @@
+public class BigO {
+
+
+
+
+
+    public void printNSquaredTimes(int n) {
+        for (int i = 1; i <= Math.pow(n, 2); i++) {
+            System.out.printf("I am %d years old\n", i);
+        }
+    }
+}
